16ee3432387ea8a6a874443a87fa4ebe9771ceca
DimitriC71 <92568593+DimitriC71@users.noreply.github.com>
2022-02-09 15:06:59 +0100

    Update LandmarksTableViewController.swift

--- CoreDataTodo/LandmarksTableViewController.swift
+++ CoreDataTodo/LandmarksTableViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import CoreData
 
 class LandmarksTableViewController: UITableViewController {
-
+    var landmarks : [Landmark] = []
     var text: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(text)
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -27,11 +32,132 @@ class LandmarksTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of sections
         return 0
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    var container : NSPersistentContainer{
+        return(UIApplication.shared.delegate as! AppDelegate).persistentContainer
     }
+    
+    private func saveContext(){
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+    }
+    private func fetchLandmark(searchQuery: String? = nil) -> [Landmark]{
+        let fetchRequest = Landmark.fetchRequest()
+        
+        let sortDescriptor = NSSortDescriptor(keyPath : \Landmark.title, ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        if let searchQuery = searchQuery, !searchQuery.isEmpty {
+            let predicate = NSPredicate(format : "%K contains[cd] %@", argumentArray: [#keyPath(Landmark.title),searchQuery])
+            fetchRequest.predicate = predicate
+        }
+        
+        
+        do{
+            let result = try container.viewContext.fetch(fetchRequest)
+            return result
+        }catch{
+            fatalError(error.localizedDescription)
+        }
+    }
+    private func createLandmark(title: String, description: String = "", date: Date = Date()){
+        let landmark = Landmark(context: container.viewContext)
+        landmark.title = title
+        landmark.creationDate = date
+        landmark.modificationDate = date
+        landmark.desc = description
+        saveContext()
+    }
+
+    private func delete(landmark : Landmark){
+        container.viewContext.delete(landmark)
+        saveContext()
+    }
+    
+    @IBAction func AddBarButtonItemAction(_ sender: UIBarButtonItem) {
+    
+        let alertController = UIAlertController(title: "nouveau lieu", message: "ajouter nouveau lieu", preferredStyle: .alert)
+        alertController.addTextField { textField in
+            textField.placeholder = "Titre…"
+        }
+        
+        let cancelAction = UIAlertAction(title : "Annuler",
+                                         style: .cancel,
+                                         handler: nil)
+        
+        let saveAction = UIAlertAction(title : "Sauvegarder",
+                                   style: .default){[weak self] _ in
+        
+            guard let self = self, let textField = alertController.textFields?.first else{
+                return
+            }
+            self.createLandmark(title: textField.text!)
+            self.landmarks = self.fetchLandmark()
+            self.tableView.reloadData()
+        }
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(saveAction)
+        present(alertController,animated: true)
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return landmarks.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let landmark = landmarks[indexPath.row]
+       // cell.accessoryType = item.isChecked ? .checkmark : .none
+        cell.textLabel?.text = landmark.title
+        cell.detailTextLabel?.text = DateFormatter.localizedString(from: landmark.creationDate!,
+                                                                   dateStyle: .short,
+                                                                   timeStyle: .short)
+        
+        
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let landmark = landmarks[indexPath.row]
+        let deleteAction = UIContextualAction(style: .destructive, title: "Supprimer"){ [weak self] _, _,completion in
+            guard let self = self else {
+                return
+            }
+        
+            self.delete(landmark: landmark)
+            //self.items = self.fetchItems()
+            self.landmarks.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        let swipeActionConfiguration = UISwipeActionsConfiguration(actions: [deleteAction])
+        return swipeActionConfiguration
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let landmark  = landmarks[indexPath.row]
+       // category.isChecked.toggle()
+        saveContext()
+        guard let cell = tableView.cellForRow(at: indexPath) else {
+            return
+        }
+       // cell.accessoryType = category.isChecked ? .checkmark : .none
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "CategoryToLandmark" , let destinationViewController = segue.destination as? LandmarksTableViewController{
+            destinationViewController.text = "Landmarks"
+        }
+    }
+    
+}
+
+extension LandmarksTableViewController : UISearchResultsUpdating{
+     
+    func updateSearchResults(for seatchController : UISearchController){
+        let searchQuery = seatchController.searchBar.text
+        landmarks = fetchLandmark(searchQuery: searchQuery)
+        tableView.reloadData()
+    }
+}
+
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -88,4 +214,4 @@ class LandmarksTableViewController: UITableViewController {
     }
     */
 
-}
+
